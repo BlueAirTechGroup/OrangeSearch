9472ff217ab7ce501e6db84c73588b3300377f3e
ToiletCommender <cxcyhdyx@sina.com>
2017-10-13 23:42:04 -0400

    Layout Fix
    Make the texts below the search box align to the right with the search box

--- cn/index.php
+++ cn/index.php
@@ -36,7 +36,7 @@
                 border-radius:0;
                 height:47px;
                 line-height:50px;
-                font-size:34px;
+                font-size:24px;
                 padding:0;
                 margin:0;
                 vertical-align:top;
@@ -59,18 +59,29 @@
 				<div class="row row-as-base">
 					<div class="col col-tablet-9">
         					<div class="container">
-                    				<form method="post" action="search.php">
-                    					<p style="font-size:0;margin-bottom:10px;">
-                    						<span class="display-normal-tablet" style="font-size:34px;line-height:50px;margin-right:4px;">橙子搜索</span>
-                    						<input class="bg-orange" type="text" name="searchKeyword" id="searchKeyword" placeholder="在此输入搜索内容..."></input>
-                    						<input type="submit" class="backgroundimg-cover" name="SearchBtn" id="SearchBtn"></input>
-                    					</p>
-                    				</form>
-                    				<p class="lead text-right">你的搜索,从未如此安全和安心</p>
-                    				<p class="text-right">截止到现在, 有<?php echo $myMySQLCls->checkExist($searchDBConn, 'SearchRstList', array());  ?>个URL被收录</p>
-                    				<p class="text-right">想看看我们的<a class="text-white" href="whyorangesearch.html">核心理念</a>?</p>
-                    				<p class="text-right small">Test Version[0000.0002 Alpha]</p>
-                    				<p class="text-right"><a class="text-white" href="../?selectLang=true">Choose Language</a></p>
+        							<div class="row row-as-base">
+        								<div class="col col-tablet-1 col-comp-2 invisible-phone"></div>
+        								<div class="col col-tablet-10 col-comp-8">
+        									<!-- div class="row row-as-base" -->
+        										<form method="post" action="search.php">
+                                					<p style="font-size:0;margin-bottom:10px;display:block;">
+                                						<span class="display-normal-tablet" style="font-size:42px;line-height:50px;margin-right:10px;">橙子搜索</span>
+                                						<span class="float-right" style="display:inline-block;position:static;">
+                                							<input class="bg-orange" type="text" name="searchKeyword" id="searchKeyword"></input>
+                                							<input type="submit" class="backgroundimg-cover" name="SearchBtn" id="SearchBtn"></input>
+                                						</span>
+                                					</p>
+                                				</form>
+                                				<div class="after-float"></div>
+                                				<p class="lead text-right">你的搜索,从未如此安全和安心</p>
+                                				<p class="text-right">截止到现在, 有<?php echo $myMySQLCls->checkExist($searchDBConn, 'SearchRstList', array());  ?>个URL被收录</p>
+                                				<p class="text-right">想看看我们的<a class="text-white" href="whyorangesearch.html">核心理念</a>?</p>
+                                				<p class="text-right small">Test Version[0000.0002 Alpha]</p>
+                                				<p class="text-right"><a class="text-white" href="../?selectLang=true">Choose Language</a></p>
+        									<!-- /div -->
+        								</div>
+        								<div class="col col-tablet-1 col-comp-2 invisible-phone"></div>
+                    				</div>
         					</div>
 					</div>
 				</div>
--- en/index.php
+++ en/index.php
@@ -36,7 +36,7 @@
                 border-radius:0;
                 height:47px;
                 line-height:50px;
-                font-size:34px;
+                font-size:24px;
                 padding:0;
                 margin:0;
                 vertical-align:top;
@@ -59,18 +59,29 @@
 				<div class="row row-as-base">
 					<div class="col col-tablet-9">
         					<div class="container">
-                    				<form method="post" action="search.php">
-                    					<p style="font-size:0;margin-bottom:10px;">
-                    						<span class="display-normal-tablet" style="font-size:34px;line-height:50px;margin-right:4px;">Orange Search</span>
-                    						<input class="bg-orange" type="text" name="searchKeyword" id="searchKeyword" placeholder="Enter Search Content Here..."></input>
-                    						<input type="submit" class="backgroundimg-cover" name="SearchBtn" id="SearchBtn"></input>
-                    					</p>
-                    				</form>
-                    				<p class="lead text-right">Your Search has never been so secure and private</p>
-                    				<p class="text-right">Until now, <?php echo $myMySQLCls->checkExist($searchDBConn, 'SearchRstList', array());  ?> search results are available</p>
-                    				<p class="text-right">Want to see our <a class="text-white" href="whyorangesearch.html">Major Concepts</a>?</p>
-                    				<p class="text-right small">Test Version[0000.0002 Alpha]</p>
-                    				<p class="text-right"><a class="text-white" href="../?selectLang=true">Choose Language</a></p>
+        							<div class="row row-as-base">
+        								<div class="col col-tablet-1 col-comp-2 invisible-phone"></div>
+        								<div class="col col-tablet-10 col-comp-8">
+        									<!-- div class="row row-as-base" -->
+        										<form method="post" action="search.php">
+                                					<p style="font-size:0;margin-bottom:10px;display:block;">
+                                						<span class="display-normal-tablet" style="font-size:42px;line-height:50px;margin-right:10px;">Orange Search</span>
+                                						<span class="float-right" style="display:inline-block;position:static;">
+                                							<input class="bg-orange" type="text" name="searchKeyword" id="searchKeyword"></input>
+                                							<input type="submit" class="backgroundimg-cover" name="SearchBtn" id="SearchBtn"></input>
+                                						</span>
+                                					</p>
+                                				</form>
+                                				<div class="after-float"></div>
+                                				<p class="lead text-right">Your search has never been so secure, and safe</p>
+                                				<p class="text-right">Until now, <?php echo $myMySQLCls->checkExist($searchDBConn, 'SearchRstList', array());  ?> search results are available</p>
+                                				<p class="text-right">Want to see our <a class="text-white" href="whyorangesearch.html">Major Concepts</a>?</p>
+                                				<p class="text-right small">Test Version[0000.0002 Alpha]</p>
+                                				<p class="text-right"><a class="text-white" href="../?selectLang=true">Choose Language</a></p>
+        									<!-- /div -->
+        								</div>
+        								<div class="col col-tablet-1 col-comp-2 invisible-phone"></div>
+                    				</div>
         					</div>
 					</div>
 				</div>
